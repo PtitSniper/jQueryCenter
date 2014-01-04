08b84ae2e9da745753f6c377dbe2f0f62cab78f3
PtitSniper <ul.ptitsniper@gmail.com>
2014-01-04 17:17:12 +0100

    Update center.js

--- center.js
+++ center.js
@@ -1,9 +1,12 @@
+/*  PtitSniper    */
+/*  Version 1.0   */
+/*  jQueryCenter  */
 jQuery.fn.center=function(){
-		$(this).css({
-			"position" : "fixed",
-			"left":"50%",
-			"top":"50%",
-			"margin-left" : - $(this).width() / 2 + "px",
-			"margin-top" :  - $(this).height() / 2 + "px"
-		});
+	$(this).css({
+		"position" : "fixed",
+		"left":"50%",
+		"top":"50%",
+		"margin-left" : - $(this).width() / 2 + "px",
+		"margin-top" :  - $(this).height() / 2 + "px"
+	});
 };
